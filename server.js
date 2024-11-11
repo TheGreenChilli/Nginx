4a71411ae251f37372047a73e7132b0da6a40f3c
TheGreenChilli <91822886+TheGreenChilli@users.noreply.github.com>
2024-11-11 09:14:56 +0000

    server

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const port = 3000
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   path.join(__dirname, req.url === '/' ? "index.html" : "req.url")
 
-  string(path.extname(filePath)).tolowerCase()
+  const extName = string(path.extname(filePath)).tolowerCase()
 
   const mimeTypes = {
     '.html': 'text/html',
     '.css': 'text/css',
     '.js': 'text/javascript',
-    '.png': 'text/png'
+    '.png': 'text/png',
   }
  
-  mimeTypes[extName] || 'application/octate-stream'
+ const contentType = mimeTypes[extName] || 'application/octate-stream'
+ 
+ fs.readFile(filePath, (err, content) => {
+  if(err) {
+    if(err.code == "ENOENT"){
+      res.writeHead
+      (404, {'Content-Type': 'text/plain'});
+      res.end("400: File Not Found");
+    }
 
+  } else {
+    res.writeHead(200, {"content-Type": contentType});
+    res.end(content, "utf-8");
+
+  }
+ });
 }); 
 
 server.listen(port, () => {
